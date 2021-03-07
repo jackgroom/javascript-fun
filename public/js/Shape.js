55a32a4d24fb5a1332c4d2dab8df325697ac21f9
55797559 <55797559+jackgroom@users.noreply.github.com>
2021-03-07 04:42:17 +0000

    updated shape to be an object library

--- public/js/Shape.js
+++ public/js/Shape.js
@@ -1,20 +1,19 @@
-export default function Shape(context) {
-  this.ctx = context;
-  line = (xPos, yPos, xStart, yEnd) => {
-    ctx.beginPath(this.ctx);
-    ctx.moveTo(xPos, yPos);
-    ctx.lineTo(xStart, yEnd);
-    ctx.stroke();
-    ctx.closePath();
-  };
+export default {
+  line: (context, xPos, yPos, xStart, yEnd) => {
+    context.beginPath();
+    context.moveTo(xPos, yPos);
+    context.lineTo(xStart, yEnd);
+    context.stroke();
+    context.closePath();
+  },
 
-  rect = (x, y, w, h, c) => {
-    ctx.fillStyle = c ? c : 'white';
-    ctx.fillRect(x, y, w, h);
-  };
+  rect: (context, x, y, w, h, c) => {
+    context.fillStyle = c ? c : 'white';
+    context.fillRect(x, y, w, h);
+  },
 
-  ellipse = (xPos, yPos, r) => {
-    ctx.beginPath();
-    ctx.arc(xPos, yPos, r, 0, Math.PI * 2, false);
-  };
-}
+  ellipse: (context, xPos, yPos, r) => {
+    context.beginPath();
+    context.arc(xPos, yPos, r, 0, Math.PI * 2, false);
+  },
+};
